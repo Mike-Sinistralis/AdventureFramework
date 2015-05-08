3a7a82cada11ba2260f0f12740dd4fa0d425010b
Michael Zimmerman <mzimmerman@turningtechnologies.com>
2015-05-08 02:31:02 -0400

    Messing with drawing rects and new resource bar

--- src/main/java/com/adventureframework/core/gui/AdventureHealth.java
+++ src/main/java/com/adventureframework/core/gui/AdventureHealth.java
@@ -1,13 +1,14 @@
 package com.adventureframework.core.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import org.lwjgl.opengl.GL11;
 
 class AdventureHealth extends Draggable {
 
-    private static final ResourceLocation HEALTHBAR = new ResourceLocation("adventureframework","textures/gui/healthbar.png");
+    private static final ResourceLocation HEALTHBAR = new ResourceLocation("adventureframework","textures/gui/resourceBar.png");
 
     public AdventureHealth(Minecraft mc, RenderGameOverlayEvent.ElementType element)
     {
@@ -22,7 +23,13 @@ class AdventureHealth extends Draggable {
 
         this.mc.getTextureManager().bindTexture(HEALTHBAR);
 
-        this.drawTexturedModalRect(
-                event.resolution.getScaledWidth()/2 - 52, event.resolution.getScaledHeight()/2 - 5, 0, 0, 106, 10);
+        //this.drawTexturedModalRect(
+          //      event.resolution.getScaledWidth()/2 - 53, event.resolution.getScaledHeight()/2 - 5, 0, 0, 92, 8);
+
+        Gui.drawRect(event.resolution.getScaledWidth()/2 - 46, event.resolution.getScaledHeight()/2 - 4, event.resolution.getScaledWidth()/2 + 46, event.resolution.getScaledHeight()/2 - 4, 0xFFFF0000);
+
+        Gui.drawRect(0,0, 100, 10, 0xFFFF0000);
+
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
